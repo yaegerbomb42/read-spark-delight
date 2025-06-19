99bab75d4f96ccc61d8b2c07406fa1aef08634fb
yaegerbomb42 <yaeger.james42@gmail.com>
2025-06-19 00:24:59 -0500

    Add reading timer and stats tracking

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,18 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <StatsProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/read/:bookId" element={<BookReaderView />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </StatsProvider>
     </TooltipProvider>
   </QueryClientProvider>
 );
